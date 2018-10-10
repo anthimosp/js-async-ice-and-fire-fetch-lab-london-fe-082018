5652fbdd6912042c80a97cf640c0f1e82318bdd9
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-10 19:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-  fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(json => renderBooks())
+  fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
